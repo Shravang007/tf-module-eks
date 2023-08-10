854ab06585e410e01742c9af6f04bd15011a714d
Shravang007 <shravangoudgajula@gmail.com>
2023-08-10 10:05:21 +0530

    eks

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "aws_eks_cluster" "eks" {
+  name     = "${var.env}-eks"
+  role_arn = aws_iam_role.eks-role.arn
+
+  vpc_config {
+    subnet_ids     = var.subnet_ids
+}
+}
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "${var.env}-ng"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = var.subnet_ids
+  capacity_types  = var.capacity_types
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.min_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+  }
+
+
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "capacity_types" {}
+variable "instance_types" {}
